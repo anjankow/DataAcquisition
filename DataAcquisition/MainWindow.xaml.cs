3bed079706c787f17ec5ac7a1765f13813711f5b
Anna Jankowska <anjankow@protonmail.com>
2018-11-24 20:24:49 +0100

    Save to CSV file

--- DataAcquisition/MainWindow.xaml.cs
+++ DataAcquisition/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO.Ports;
-using System.Diagnostics;
+using Ookii.Dialogs.Wpf;
 
 namespace DataAcquisition
 {
@@ -24,23 +11,26 @@ namespace DataAcquisition
     public partial class MainWindow : Window
     {
         public SerialPort serialPort;
-        char[] ADC1_data; 
+        public Int16[] ADC1_rawData= { 56, 9865, 4500 };
+        public Int16[] ADC2_rawData = { 656, 55, 5, 4, 1 };
+        public Int16[] ADC3_rawData = { 0 };
+
         public MainWindow()
         {
             InitializeComponent();
+
             DataAcquisition.DataContext.Port = String.Empty;
             DataAcquisition.DataContext.HowManyBuffers = 100;
             DataAcquisition.DataContext.HowManyADC = 1;
             DataAcquisition.DataContext.Frequency = 1000;
             DataAcquisition.DataContext.BufferSize = 1600;
-
             /*if(!ChooseAndOpenPort())
             {
                 //port choice window closed, end of program
                 this.Close();
             }*/
-            
-            
+
+
         }
 
         private void ReceiveMesaurementsData(object sender, SerialDataReceivedEventArgs e)
@@ -115,8 +105,41 @@ namespace DataAcquisition
                 ChooseAndOpenPort();
             }
         }
-        
-        
+
+        private void Btn_saveToCsv_Click(object sender, RoutedEventArgs e)
+        {
+            var saveDialog = new Microsoft.Win32.SaveFileDialog();
+            VistaFolderBrowserDialog dlg = new VistaFolderBrowserDialog();
+            dlg.ShowNewFolderButton = true;
+            if(dlg.ShowDialog() != true)
+            {
+                return;
+            }
+            string dateTimeNow = DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year + "_" + String.Format("{0:D2}",DateTime.Now.Hour) + " -" + String.Format("{0:D2}", DateTime.Now.Minute);
+            string fileName1 = dlg.SelectedPath + "\\ADC1_pomiary_" + dateTimeNow  + ".csv";
+            string fileName2 = dlg.SelectedPath + "\\ADC2_pomiary_" + dateTimeNow + ".csv";
+            string fileName3 = dlg.SelectedPath + "\\ADC3_pomiary_" + dateTimeNow + ".csv";
+
+            using (var streamWriter = new System.IO.StreamWriter(fileName1,false))
+            {
+                streamWriter.Write("frequency," + DataAcquisition.DataContext.Frequency.ToString() + ",Hz\n\n");
+                var csvWriter = new CsvHelper.CsvWriter(streamWriter);
+                csvWriter.WriteRecords(ADC1_rawData);
+            }
+            using (var streamWriter = new System.IO.StreamWriter(fileName2, false))
+            {
+                streamWriter.Write("frequency," + DataAcquisition.DataContext.Frequency.ToString() + ",Hz\n\n");
+                var csvWriter = new CsvHelper.CsvWriter(streamWriter);
+                csvWriter.WriteRecords(ADC2_rawData);
+            }
+            using (var streamWriter = new System.IO.StreamWriter(fileName3, false))
+            {
+                streamWriter.Write("frequency," + DataAcquisition.DataContext.Frequency.ToString() + ",Hz\n\n");
+                var csvWriter = new CsvHelper.CsvWriter(streamWriter);
+                csvWriter.WriteRecords(ADC3_rawData);
+            }
+            MessageBox.Show("Zapisano wyniki we wskazanej lokalizacji", "Wyniki zapisane", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
 
